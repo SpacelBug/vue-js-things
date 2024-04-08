b73927115eda5ce37055f5bbeb4dd6691af93421
I love kludge <98ger@mail.ru>
2024-04-08 16:50:24 +1200

    add new component MultiSelect

---
+++ src/components/basic/multi-select/MultiSelect.vue
@@ -0,0 +1,129 @@
+<template>
+  <div
+      id="MultiSelect"
+      tabindex="0"
+      @focusout="isShowOptions = false"
+      @focusin="isShowOptions = true"
+  >
+    <div class="select-button">
+      <div v-if="modelValue" class="value" :title="modelValue.join(', ')">{{ modelValue.join(', ') }}</div>
+      <div v-else class="placeholder">{{ placeHolder }}</div>
+      <div :class="['arrow-head-down-icon', {'arrow-head-down-icon-rotate': isShowOptions}]"/>
+    </div>
+    <div class="options" v-if="isShowOptions">
+      <div
+          v-for="(option, index) in options"
+          :class="['option', {'active': selectedOptionsIndexes.has(Number(index))}]"
+          @click="onOptionClick(index)"
+      >
+        {{ option }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MultiSelect",
+  props: {
+    modelValue: {type: Array, default: null},
+    options: {type: Array, default: new Array([])},
+
+    placeHolder: {type: String, default: "Select values"},
+
+    backgroundColor: {type: String, default: "grey"},
+    activeColor: {type: String, default: "lightgrey"},
+    border: {type: String, default: "none"},
+    boxShadow: {type: String, default: "none"},
+    optionsBoxHeight: {type: Number, default: 280},
+  },
+  computed: {
+    selectedOptions() {
+      let selectedOptions = Array.from(this.selectedOptionsIndexes).map((index)=>{
+        return this.options[index]
+      })
+      return selectedOptions.length > 0 ? selectedOptions : null
+    },
+  },
+  data() { return {
+    selectedOptionsIndexes: new Set(),
+    isShowOptions: false,
+  }},
+  methods: {
+    onOptionClick(index) {
+      this.selectedOptionsIndexes.has(index) ? this.selectedOptionsIndexes.delete(index) : this.selectedOptionsIndexes.add(index)
+      this.$emit('update:modelValue', this.selectedOptions)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#MultiSelect{
+  position: relative;
+  user-select: none;
+}
+.select-button{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: v-bind(backgroundColor);
+  border: v-bind(border);
+  box-shadow: v-bind(boxShadow);
+  height: fit-content;
+  width: 100%;
+  cursor: pointer;
+  padding: 4px 16px;
+}
+.value{
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+/***OptionsClasses***/
+.options{
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  height: v-bind(optionsBoxHeight + 'px');
+  background-color: v-bind(backgroundColor);
+  border: v-bind(border);
+  box-shadow: v-bind(boxShadow);
+  width: calc(100% + 16px * 2);
+  padding: 0;
+  overflow-y: auto;
+}
+.option{
+  cursor: pointer;
+  padding: 4px 16px;
+}
+.active{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.active::after{
+  display: block;
+  content: '';
+  height: 8px;
+  width: 8px;
+  border-radius: 8px;
+  background-color: v-bind(activeColor);
+}
+/***Icons***/
+.arrow-head-down-icon{
+  cursor: pointer;
+  background-color: v-bind($el.style.color);
+  height: 15px;
+  width: 15px;
+  aspect-ratio: 1/1;
+  mask-image: url("arrow-head-down.svg");
+  mask-size: contain;
+}
+.arrow-head-down-icon-rotate{
+  transform: rotate(180deg);
+}
+</style>
--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -5,7 +5,7 @@
       <div class="component-box-component">
         <h3>Select</h3>
         <vue-select
-            v-model="testValue"
+            v-model="testValueSelect"
             :options="options"
             style="width: 200px; color: black"
         />
@@ -15,7 +15,34 @@
         <pre>
           props:
             required:
-              modelValue - отвечает за v-model
+              modelValue - отвечает за v-model (строка)
+              options - параметры для выбора
+            optional:
+              placeholder - default = 'Select value'
+              backgroundColor - default = 'grey'
+              activeColor - default = 'lightgrey'
+              border - default = 'none'
+              boxShadow - default = 'none'
+              optionsBoxHeight - default = '280'
+        </pre>
+      </div>
+    </div>
+
+    <div class="component-box">
+      <div class="component-box-component">
+        <h3>MultiSelect</h3>
+        <multi-select
+            v-model="testValueMultiSelect"
+            :options="options"
+            style="width: 200px; color: black"
+        />
+      </div>
+      <div class="component-box-description">
+        Select с возможностью выбора нескольких значений
+        <pre>
+          props:
+            required:
+              modelValue - отвечает за v-model (список)
               options - параметры для выбора
             optional:
               placeholder - default = 'Select value'
@@ -33,15 +60,18 @@
 
 <script>
 import VueSelect from "@/components/basic/select/VueSelect";
+import MultiSelect from "@/components/basic/multi-select/MultiSelect";
 
 export default {
   name: "ComponentsView",
   components: {
     VueSelect,
+    MultiSelect,
   },
   data() { return {
-    testValue: null,
-    options: [...Array(25).keys().map((index)=>{return `Some value ${index}`})]
+    testValueSelect: null,
+    options: [...Array(25).keys().map((index)=>{return `Some value ${index}`})],
+    testValueMultiSelect: null,
   }}
 }
 </script>
