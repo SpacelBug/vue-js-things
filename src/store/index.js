a13229d37d367c537cb43aa3c99f480d8c4804ad
I love kludge <98ger@mail.ru>
2024-05-31 15:59:00 +1200

    add theme module into store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import themeModule from "@/store/modules/theme.module";
 
 export default createStore({
   state: {
@@ -10,5 +11,6 @@ export default createStore({
   actions: {
   },
   modules: {
+    themeModule
   }
 })
